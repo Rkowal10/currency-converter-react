3e3c4b4e9bba058756f419605b5805a2f9fe4d9f
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-02-23 11:04:01 +0100

    created file currencies.js with new objects

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { id: 1, name: "Dolar amerykański (USD)", value: 4.46 },
+    { id: 2, name: "Euro (EUR)", value: 4.75 },
+    { id: 3, name: "Funt szterling (GBP)", value: 5.39 },
+    { id: 4, name: "Frank szwajcarski (CHF)", value: 4.81 },
+];
+
+export {currencies};
