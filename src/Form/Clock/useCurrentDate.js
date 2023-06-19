637d3fe091da6985ca4bbc8cf2c5616347bac4c6
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-06-19 09:47:18 +0200

    deleted empty lines

--- src/Form/Clock/useCurrentDate.js
+++ src/Form/Clock/useCurrentDate.js
@@ -3,8 +3,6 @@ import { useState, useEffect } from "react";
 export const useCurrentDate = () => {
     const [date, setDate] = useState(new Date());
 
-    
-
     useEffect(() => {
         const intervalId = setInterval(() => {
             setDate(new Date());
