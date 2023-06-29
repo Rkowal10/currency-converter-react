77d0666b8553645e7c011ab8b0e1426a57d5cbeb
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-06-29 10:59:03 +0200

    deleted unnecessary file

--- src/currencies.js
+++
@@ -1,9 +0,0 @@
-const currencies = [
-    { id: 1, name: "Dolar amerykański (USD)", value: 4.46 },
-    { id: 2, name: "Euro (EUR)", value: 4.75 },
-    { id: 3, name: "Funt szterling (GBP)", value: 5.39 },
-    { id: 4, name: "Frank szwajcarski (CHF)", value: 4.81 },
-    { id: 5, name: "Polski złoty (PLN)", value: 1 },
-];
-
-export {currencies};
