cfd6436e3dd89219a8ca834c8ba09c1324a636bf
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-06-29 11:17:43 +0200

    added Loading and Error and modified display

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,28 +1,37 @@
 import { useState } from "react";
-import { currencies } from "../currencies";
 import Result from "./Result";
 import Clock from "./Clock";
-import { StyledForm, Fieldset, Legend, Label, Input, Button, Paragraph } from "./styled";
+import { useRatesData } from "./useRatesData";
+import {
+    StyledForm,
+    Fieldset,
+    Legend,
+    Label,
+    Input,
+    Button,
+    Paragraph,
+    Loading,
+    Error,
+} from "./styled";
 
 const Form = ({ title }) => {
     const [amount, setNewAmount] = useState("");
-    const [inputCurrency, setInputCurrency] = useState(currencies[0].name);
-    const [outputCurrency, setOutputCurrency] = useState(currencies[0].name);
     const [result, setResult] = useState();
+    const [outputCurrency, setOutputCurrency] = useState("GBP");
+    const ratesData = useRatesData();
 
-    const calculateResult = (amount, inputCurrency, outputCurrency) => {
-        const inputRate = currencies.find(({ name }) => name === inputCurrency).value;
-        const outputRate = currencies.find(({ name }) => name === outputCurrency).value;
+    const calculateResult = (amount, outputCurrency) => {
+        const outputRate = ratesData.rates[outputCurrency];
 
         setResult({
-            result: (amount * inputRate) / outputRate,
+            result: amount * outputRate,
             outputCurrency,
         });
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        calculateResult(amount, inputCurrency, outputCurrency);
+        calculateResult(amount, outputCurrency);
     };
 
     return (
@@ -30,60 +39,57 @@ const Form = ({ title }) => {
             <Fieldset>
                 <Legend>{title}</Legend>
                 <Clock />
-                <Label>Jaką kwotę chcesz wymienić:
-                    <Input
-                        type="number"
-                        value={amount}
-                        onChange={({ target }) => setNewAmount(target.value)}
-                        step="any"
-                        min="0"
-                        required
-                        autoFocus={true}
-                        placeholder="Wpisz kwotę"
-                    />
-                </Label>
-                <p>
-                    <Label>Wybierz walutę początkową:
-                        <Input as="select"
-                            value={inputCurrency}
-                            onChange={({ target }) => setInputCurrency(target.value)}
-                        >
-                            {currencies.map(inputCurrency => (
-                                <option
-                                    key={inputCurrency.id}
-                                    value={inputCurrency.name}
-                                >
-                                    {inputCurrency.name}
-                                </option>
-                            ))};
-                        </Input>
-                    </Label>
-                </p>
-                <p>
-                    <Label>Wybierz walutę końcową:
-                        <Input as="select"
-                            value={outputCurrency}
-                            onChange={({ target }) => setOutputCurrency(target.value)}
-                        >
-                            {currencies.map(outputCurrency => (
-                                <option
-                                    key={outputCurrency.id}
-                                    value={outputCurrency.name}
-                                >
-                                    {outputCurrency.name}
-                                </option>
-                            ))};
-                        </Input>
-                    </Label>
-                </p>
-                <Button>Oblicz</Button>
-                <Paragraph>Otrzymasz:</Paragraph>
-                <Result
-                    result={result}
-                />
-                <Paragraph smaller>
-                    Kursy walut pochądzą ze strony nbp.pl z Tabeli nr 037/A/NBP/2023 z dnia 2023-02-22
-                </Paragraph>
+                {ratesData.state === "loading" ? (
+                    <Loading>
+                        Proszę chwilę poczekać, trwa ładowanie danych!
+                    </Loading>
+                ) : (
+                    ratesData.state === "error" ? (
+                        <Error>
+                            Coś poszło nie tak!
+                        </Error>
+                    ) : (
+                        <>
+                            <Label>Podaj kwotę w PLN do wymiany:
+                                <Input
+                                    type="number"
+                                    value={amount}
+                                    onChange={({ target }) => setNewAmount(target.value)}
+                                    step="any"
+                                    min="0"
+                                    required
+                                    autoFocus={true}
+                                    placeholder="Wpisz kwotę"
+                                />
+                            </Label>
+                            <p>
+                                <Label>Wybierz walutę końcową:
+                                    <Input as="select"
+                                        value={outputCurrency}
+                                        onChange={({ target }) => setOutputCurrency(target.value)}
+                                    >
+                                        {Object.keys(ratesData.rates).map(((outputCurrency) => (
+                                            <option
+                                                key={outputCurrency}
+                                                value={outputCurrency}
+                                            >
+                                                {outputCurrency}
+                                            </option>
+                                        )))};
+                                    </Input>
+                                </Label>
+                            </p>
+                            <Button>Oblicz</Button>
+                            <Paragraph>Otrzymasz:</Paragraph>
+                            <Result
+                                result={result}
+                            />
+                            <Paragraph smaller>
+                                Kursy walut pobrane ze strony <a rel="noreferrer noopener" target="_blank" href="https://exchangerate.host">https://exchangerate.host</a>.
+                            </Paragraph>
+                        </>
+                    )
+                )}
             </Fieldset>
         </StyledForm>
     );
